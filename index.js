e001f5ef391bcabfdec4ef1f4d74bcabb3832844
MihaiMagurean <123626521+MihaiMagurean@users.noreply.github.com>
2024-04-09 20:39:41 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
+
+let age;
+age = prompt("Ce varta aveti?")
+if (age <= 18) {
+    console.log("Acces permis. Bine ați venit pe site!");
+} else {
+    console.log("Acces restricționat. Trebuie să aveți peste 18 ani pentru a accesa acest site.");
+}
+
